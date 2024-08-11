322d101fb9de94d92564f6f4fbd0cecb69ed53d0
usernamegithuber <102531047+usernamegithuber@users.noreply.github.com>
2024-08-11 23:52:20 +0530

    #CrackYourPlacement

---
+++ InveretBinaryTree.java
@@ -0,0 +1,19 @@
+package CrackYourplacementDay22;
+
+public class InveretBinaryTree {
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
+        }
+    }
+
+}
